ba4e946bd7517a675579d8b1bac4c5b147d2c666
Niko Uusitalo <niko.u@hotmail.fi>
2022-08-09 23:21:03 +0300

    Revert "Korjaus"
    This reverts commit 0228cf1eeed7448aedc58d5ddacaca3a98b9c04b, reversing
    changes made to 5c2d7192cc26c7228b93d54767271650ed8bec44.

--- app-react/src/pages/HomePages.jsx
+++ app-react/src/pages/HomePages.jsx
@@ -3,15 +3,12 @@ import Categories from '../components/home/Categories'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 import Collection from '../components/home/Collection'
 import NewArrival from '../components/home/NewArrival'
-import HomeTop from '../components/home/HomeTop'
-
 
 export class HomePages extends Component {
      render() {
           return (
                <Fragment>
                     <FeaturedProducts />
-                    <HomeTop />
                     <Categories />
                     <Collection />
                     <NewArrival />
@@ -20,5 +17,3 @@ export class HomePages extends Component {
      }
     }
     export default HomePages
-
-
