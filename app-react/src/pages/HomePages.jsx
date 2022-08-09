5096fbb65b77bd0491f37fecdc24026b5457596b
Niko Uusitalo <niko.u@hotmail.fi>
2022-08-09 22:59:52 +0300

    Teisdostoja

--- app-react/src/pages/HomePages.jsx
+++ app-react/src/pages/HomePages.jsx
@@ -1,13 +1,22 @@
 import React, { Component, Fragment } from 'react'
+import Categories from '../components/home/Categories'
+import FeaturedProducts from '../components/home/FeaturedProducts'
+import Collection from '../components/home/Collection'
+import NewArrival from '../components/home/NewArrival'
+import HomeTop from '../components/home/HomeTop'
 
 export class HomePages extends Component {
-  render() {
-    return (
-      <>
-        <h1> testi</h1>
-      </>
-    )
-  }
-}
+     render() {
+          return (
+               <Fragment>
+                    <FeaturedProducts />
+                    <HomeTop />
+                    <Categories />
+                    <Collection />
+                    <NewArrival />
+               </Fragment>
+          )
+     }
+    }
+    export default HomePages
 
-export default HomePages
