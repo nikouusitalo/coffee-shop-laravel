452c191dab7f3a8ab08ec6516ff05a83e8c23871
Niko Uusitalo <niko.u@hotmail.fi>
2022-08-12 20:50:51 +0300

    laravel api testaus

--- app-laravel/routes/api.php
+++ app-laravel/routes/api.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\Admin\VisitorController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+  //  return $request->user();
+//});
+
+
+Route::get('/getvisitor', [VisitorController::class, 'GetVisitorDetails']);
